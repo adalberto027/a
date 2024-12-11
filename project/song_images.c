f988889cd725581f326ea85755b6430c7698d95d
adalberto027 <adalberto2772003@gmail.com>
2024-12-11 08:52:01 -0700

    Update song_images.c

--- project/song_images.c
+++ project/song_images.c
@@ -2,32 +2,39 @@
 #include "lcddraw.h"
 
 void drawDefault(u_int colorBGR) {
-  // Clear the screen with a festive background color
   clearScreen(colorBGR);
-  
-  // Draw a festive title at the top
-  drawString5x7(10, 15, "Merry Christmas", COLOR_WHITE, colorBGR);
-  
-  // Get the screen dimensions
+  drawString5x7(10, 15, "The Legend of Zelda", COLOR_GOLD, colorBGR);
+  // find the height and width of the screen
   u_char width = screenWidth, height = screenHeight;
-
-  // Find the location of the top of the tree
+  // find the location of the top triangle
   int centerCol = width / 2, centerRow = height / 4;
-  int treeHeight = 40; // Height of the tree
+  // set the length it each triangle
+  int sideLength = 20; 
 
-  // Draw the main part of the tree (a large green triangle)
-  for (int row = 0; row < treeHeight; row++) {
+  // draw the top triangle
+  for (int row = 0; row < sideLength; row++) {
     for (int col = -row; col <= row; col++) {
-      drawPixel(centerCol + col, centerRow + row, COLOR_GREEN);
+      drawPixel(centerCol + col, centerRow + row, COLOR_GOLD);
     }
   }
 
-  // Draw the tree trunk (a small brown rectangle)
-  int trunkHeight = 10, trunkWidth = 6;
-  int trunkCenterRow = centerRow + treeHeight;
-  for (int row = 0; row < trunkHeight; row++) {
-    for (int col = -trunkWidth / 2; col <= trunkWidth / 2; col++) {
-      drawPixel(centerCol + col, trunkCenterRow + row, COLOR_BROWN);
+  // draw the bottom-left triangle
+  // find the location of the triangle
+  int leftCenterCol = centerCol - sideLength;
+  int leftCenterRow = centerRow + sideLength;
+  for (int row = 0; row < sideLength; row++) {
+    for (int col = -row; col <= row; col++) {
+      drawPixel(leftCenterCol + col, leftCenterRow + row, COLOR_GOLD);
+    }
+  }
+
+  // draw the bottom-right triangle
+  // find the location of the triangle
+  int rightCenterCol = centerCol + sideLength;
+  int rightCenterRow = centerRow + sideLength;
+  for (int row = 0; row < sideLength; row++) {
+    for (int col = -row; col <= row; col++) {
+      drawPixel(rightCenterCol + col, rightCenterRow + row, COLOR_GOLD);
     }
   }
 }
