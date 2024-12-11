bba89a55745190b63ff26b08f2eb8ee9f8b8446c
adalberto027 <adalberto2772003@gmail.com>
2024-12-11 01:30:55 -0700

    Update sr.s

--- project/timerLib/sr.s
+++ project/timerLib/sr.s
@@ -1,27 +1,41 @@
-	.arch msp430g2553
-	.p2align 1,0
-	.text
+.arch msp430g2553
+.p2align 1,0
+.text
 
-	;;  routines to access sr
-
-	.global set_sr
-set_sr:	nop
+;; Routines to manipulate special registers
+.global set_sr
+set_sr: nop
 	mov r12, r2
 	nop
 	ret
 
-	.global get_sr
-get_sr:	mov r2, r12
+.global get_sr
+get_sr: mov r2, r12
 	ret
 
-	.global or_sr
-or_sr:	nop
+.global or_sr
+or_sr: nop
 	bis r12, r2
 	nop
 	ret
-	
-	.global and_sr
-and_sr:	nop
+
+.global and_sr
+and_sr: nop
 	and r12, r2
+	nop
+	ret
+
+;; New functionality: Toggle LED brightness level
+.global toggle_led_brightness
+toggle_led_brightness:
+	nop
+	cmp #0, r12
+	jeq low_brightness
+	high_brightness:
+	mov #0xFF, &P1OUT
+	jmp exit
+	low_brightness:
+	mov #0x55, &P1OUT
+	exit:
 	nop
 	ret
