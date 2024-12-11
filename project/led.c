115b43b39056ca5da83494fee96dc4b584cbca0b
adalberto027 <adalberto2772003@gmail.com>
2024-12-11 09:08:11 -0700

    Update led.c

--- project/led.c
+++ project/led.c
@@ -20,44 +20,44 @@ void led_update() {
   if (led_altered) {
     char ledFlags = redVal[r_led_on] | greenVal[g_led_on];     // find the desired state for the leds (0 or 1)
     // apply the ledFlags values without changing the other pins to output (leds)
-    P1OUT &= (0xff ^ LEDS) | ledFlags;                        // Clear the LED states
-    P1OUT |= ledFlags;                                        // Update the LEDs to their new state
-    led_altered = 0;                                          // Reset the altered flag
+    P1OUT &= (0xff^LEDS) | ledFlags;                           // 0xff^LEDS - 1111 1111 XOR 0100 0001 = 1011 1110
+    P1OUT |= ledFlags;                                         // update the leds to their new state (on or off)
+    led_altered = 0;                                           // reset the state
   }
 }
 
 // turns on the green led
 void green_led_on() {
-  P1OUT |= LED_GREEN;               // Turn on the green LED
-  P1OUT &= ~LED_RED;                // Ensure the red LED is off
-  __delay_cycles(500000);           // Delay for 500,000 clock cycles
+  P1OUT |= LED_RED;                     // set bit for the green led (turning it on)
+  __delay_cycles(500000);               // delay of 500000 clock cycles
 }
 
 // turns on the red led
 void red_led_on() {
-  P1OUT |= LED_RED;                 // Turn on the red LED
-  P1OUT &= ~LED_GREEN;              // Ensure the green LED is off
-  __delay_cycles(500000);           // Delay for 500,000 clock cycles
+  P1OUT |= LED_GREEN;                   // set bit for the red led (turning it on)
+  __delay_cycles(500000);               // delay of 500000 clock cycles
 }
 
 // turns off both leds
 void leds_off() {
-  P1OUT &= ~LED_GREEN;              // Turn off the green LED
-  P1OUT &= ~LED_RED;                // Turn off the red LED
-  __delay_cycles(500000);           // Delay for 500,000 clock cycles
+  // turn off the green led without changing the other pins
+  P1OUT &= ~LED_GREEN;                  // ~LED_GREEN inverts bits
+  __delay_cycles(500000);               // delay of 500000 clock cycles
+  P1OUT &= ~LED_RED;
 }
 
 // flashes the leds
 void led_flash(int n) {
-  // flash LEDs until n
+  // flash leds until n
   for (int i = 0; i < n; i++) {
-    red_led_on();                   // Turn on the red LED
-    __delay_cycles(1500000);        // Delay for visibility
-    leds_off();                     // Turn off LEDs
-    __delay_cycles(500000);         // Delay
-    green_led_on();                 // Turn on the green LED
-    __delay_cycles(1500000);        // Delay for visibility
-    leds_off();                     // Turn off LEDs
-    __delay_cycles(500000);         // Delay
+    P1OUT |= LED_GREEN;                 // turn the gren led on
+    __delay_cycles(1500000);            // delay
+    P1OUT &= ~LED_RED;                  // turn off the red led
+    __delay_cycles(500000);             // delay
+    P1OUT &= ~LED_GREEN;                // turn off the green led
+    __delay_cycles(500000);             // delay
+    P1OUT |= LED_RED;                   // turn the green led on
+    __delay_cycles(1500000);            // delay
   }
+  leds_off();                           // turn off the leds
 }
